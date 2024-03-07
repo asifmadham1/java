c1045384b591d8ff340071ec7afe2ae5d7436396
asifmadham <asifmadham2980@gmail.com>
2024-03-07 19:22:11 +0500

    7-3-24

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
-alert("Thanks for your input!");
-alert ("i am asif 'the wordpress website developer'");
-var name ="asif"
-alert (name)
-name ="asif  the WordPress Website Developer"; 
-alert(name);
-var name = "mark";
-alert (name);
-name='John Doe';
-alert (name);
+// alert("Thanks for your input!");
+// alert ("i am asif 'the wordpress website developer'");
+// var name ="asif"
+// alert (name)
+// name ="asif  the WordPress Website Developer"; 
+// alert(name);
+// var name = "mark";
+// alert (name);
+// name='John Doe';
+// alert (name);
+// <-------------------------------------//7-3-2024----------------------------->
+var nationality;
+nationality = "U.S.";
+alert(nationality);
+var nationality;
+nationality="pakistani";
+alert(nationality);
+var floogle = "Mark";
+alert(floogle);
+var lessonAuthor = "Mark";
+var guyWhoKeepsSayingHisOwnName = "Mark";
+alert(guyWhoKeepsSayingHisOwnName )
+var x = "Asif";
+alert(x)
+var lastName = "Smith";
+var cityOfOrigin = "New Orleans";
+var aussieGreeting = "g'Day";
+alert(lastName);
+alert(cityOfOrigin);
+alert(aussieGreeting);
