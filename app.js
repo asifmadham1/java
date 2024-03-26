bf54376db76d8d620876407be39225204c937a5c
asifmadham <asifmadham2980@gmail.com>
2024-03-26 19:43:42 +0500

    nested loop practice

--- app.js
+++ app.js
@@ -174,10 +174,20 @@
 
 
 
-for(var  a = 1; a<=100; a =a+10){  
+for(var  a = 1; a<=100; a =a+10){ 
  for(var  b = a; b < a+10 ; b++){
   document.write(b +" ");
  }
  
  document.write("<br>");
 }
+
+
+
+ 
+
+
+
+
+
+
