1bab239bcf241a4e0caea607fffe26be3de7a959
asifmadham <asifmadham2980@gmail.com>
2024-04-23 15:56:03 +0500

    assignment #1

--- app.js
+++ app.js
@@ -1,22 +1,5 @@
-var inp = prompt("find user by hobby");
-var users = [
-  { name: "ali", hobbies: ["cricket", "painting", "gaming"] },
-  {
-    name: "ahmed",
-    hobbies: ["football", "bedminton", "ludo", "hockey", "travelling"],
-  },
-  { name: "sahir", hobbies: ["cycling", "swimming", "football"] },
-];
-var flag = false;
-for (var i = 0; i < users.length; i++) {
-  for (j = 0; j < users[i].hobbies.length; j++) {
-    if (inp === users[i].hobbies[j]) {
-      flag=true;
-      alert(users[i].name);
-      break;
-    }
-  }
-}
-if (!flag) {
-  alert("hobby not found");
-}
+// Declare a variable called age & assign to it your age. Show
+// your age in an alert box
+var age = 43
+alert("i am  "+age+" years of age")
+
