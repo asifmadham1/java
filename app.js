4b7e9e3b725729915cc9479ff1f5c68d68b6da06
asifmadham <asifmadham2980@gmail.com>
2024-04-23 21:00:46 +0500

    assignment #3

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 // Declare a variable called age & assign to it your age. Show
 // your age in an alert box
-var age = 43
-alert("i am  "+age+" years of age")
+// var age = 43
+// alert("i am  "+age+" years of age");
 
+// var visit =15;
+// alert("you have visited our site "+visit+" times");
+
+// var birthYear = 1980;
+// alert("this is your birth year "+birthYear);
+// Store visitor's name, product title and quantity in variables
+var visitorName = "John Doe";
+var productTitle = "T-shirt";
+var quantity = 5;
+
+// Construct the message
+var message = visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store";
+
+// Display the message in the browser
+document.write(message);
